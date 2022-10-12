526f70934d4c734e6183735ca97e048719410b2f
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-10-12 14:20:32 +0200

    add-layout

---
+++ csharp-boolflix/Controllers/EditorController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace csharp_boolflix.Controllers
+{
+    public class EditorController : Controller
+    {
+        public IActionResult Index ()
+        {
+            return View();
+        }
+        public IActionResult Film()
+        {
+            return View();
+        }
+
+        public IActionResult TvSeries()
+        {
+            return View();
+        }
+
+        public IActionResult Genres()
+        {
+            return View();
+        }
+
+        public IActionResult Features()
+        {
+            return View();
+        }
+
+        public IActionResult Actors()
+        {
+            return View();
+        }
+    }
+}
